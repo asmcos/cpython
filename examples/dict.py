1590ee7cd3576bdeac9fb0ad2e14584c48164e9e
asmcos <asmcos@gmail.com>
2018-07-20 18:36:59 +0800

    更新dict 代码

--- examples/dict.py
+++ examples/dict.py
@@ -1,1 +1,19 @@
 #coding:utf-8
+
+d = {}
+
+
+d['a']    = 1
+d['b']    = "hello"
+d['name'] = "Jike"
+d['age']  = 21
+
+print(d)
+
+""" 第二段 """
+
+for k in d.keys():
+    """ key """
+    print (k),
+    """ value """
+    print (d[k])
